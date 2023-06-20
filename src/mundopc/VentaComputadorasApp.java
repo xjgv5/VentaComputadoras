89fa7e264579b946b6122981c0f0d4b48ad01583
Javier Gomez <javier.gov@outlook.es>
2023-06-19 21:41:44 -0600

    [UPDATE] Mejora en el sistema de pedidos

--- src/mundopc/VentaComputadorasApp.java
+++ src/mundopc/VentaComputadorasApp.java
@@ -16,23 +16,73 @@ public class VentaComputadorasApp {
         int opcion;
         Scanner consola = new Scanner(System.in);
         boolean salir = false;
+        Computadora computadora;
+        Orden orden1 = new Orden();
+
         while (!salir){
             mostrarMenu();
             opcion = Integer.parseInt(consola.nextLine());
             switch (opcion){
                 case 1:
-                    System.out.println("Ingresaste a la opcion 1");
+                    //System.out.println("Ingresaste a la opcion 1");
+                    try {
+                        computadora = crearPedido(consola);
+                        orden1.agregarComputadora(computadora);
+                    } catch (Error e){
+                        System.out.println("Oops ! ha ocurrido un error " + e);
+                    }
                     break;
+
                 case 2:
-                    System.out.println("Ingresaste a la opcion 2");
+                    System.out.println("Mostrando las ordenes actuales");
+                    orden1.mostrarOrden();
                     break;
                 case 3:
                     System.out.println("Hasta pronto ...");
                     salir = true;
                     break;
+                default:
+                    System.out.println("Opcion no valida...");
 
             }
         }
+    }
+
+    public static Computadora crearPedido(Scanner consola){
+        String tipoEntrada;
+        String marca;
+        double tamanio;
+        String nombreComputadora;
+
+        System.out.println("Iniciando pedido...");
+        System.out.println("************************************************");
+        System.out.println("Coloca las especificacion de tu ratón : ");
+        System.out.print("Tipo de entrada: ");
+        tipoEntrada = consola.nextLine();
+        System.out.print("Marca: ");
+        marca = consola.nextLine();
+        //crear el objeto
+        Raton raton  = new Raton(tipoEntrada, marca);
+        System.out.println("************************************************");
+        System.out.println("Coloca las especificacion de tu monitor : ");
+        System.out.print("Tamaño (En pulgadas): ");
+        tamanio = Double.parseDouble(consola.nextLine());
+        System.out.print("Marca: ");
+        marca = consola.nextLine();
+        Monitor monitor = new Monitor(marca, tamanio);
+        System.out.println("************************************************");
+        System.out.println("Coloca las especificaciones de tu teclado");
+        System.out.print("Tipo de entrada: ");
+        tipoEntrada = consola.nextLine();
+        System.out.print("Marca: ");
+        marca = consola.nextLine();
+        Teclado teclado = new Teclado(tipoEntrada, marca);
+        // ----------------------------- Creando la computadora ---------------------------
+        System.out.print("Ingresa el nombre de la computadora: ");
+        nombreComputadora = consola.nextLine();
+        System.out.println("Pedido creado con éxito");
+        return new Computadora(nombreComputadora, monitor, teclado, raton);
+
     }
 
     public static void mostrarMenu() {
